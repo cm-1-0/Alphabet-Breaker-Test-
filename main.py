6b84528ee7c84635de71631bf20c349906341f04
cm-1-0 <54222924+cm-1-0@users.noreply.github.com>
2021-02-05 20:53:40 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+p1 = input("Enter a phrase: ")
+p2 = list(p1)
+
+
+def phrase_calc():
+    dictionary = {
+        " ": 0,
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+        "e": 5,
+        "f": 6,
+        "g": 7,
+        "h": 8,
+        "i": 9,
+        "j": 10,
+        "k": 11,
+        "l": 12,
+        "m": 13,
+        "n": 14,
+        "o": 15,
+        "p": 16,
+        "q": 17,
+        "r": 18,
+        "s": 19,
+        "t": 20,
+        "u": 21,
+        "v": 22,
+        "w": 23,
+        "x": 24,
+        "y": 25,
+        "z": 26
+    }
+    for item in p1:
+        for letter in item:
+            p2.append(letter)
+
+
+phrase_calc()
